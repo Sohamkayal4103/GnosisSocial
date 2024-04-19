2fbbaf20f1a2efbc677268ec20d1093e0b2e35e0
SarveshLimaye <sarveshlimaye2002@gmail.com>
2024-04-20 00:10:04 +0530

    created routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,18 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home/Home";
+import CreateDAO from "./pages/CreateDAO/CreateDAO";
+import RegisterUser from "./pages/RegisterUser/RegisterUser";
 import "./App.css";
 
 function App() {
   return (
-    <>
-      <h2>Hello</h2>
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/create-dao" element={<CreateDAO />} />
+        <Route path="/register" element={<RegisterUser />} />
+      </Routes>
+    </Router>
   );
 }
 
