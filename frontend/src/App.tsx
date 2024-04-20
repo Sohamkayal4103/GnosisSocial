c8a50e623e0182f8ea53cee51ff30dac499e069b
SarveshLimaye <sarveshlimaye2002@gmail.com>
2024-04-20 10:32:17 +0530

    auth using dynamic

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,17 +2,50 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import CreateDAO from "./pages/CreateDAO/CreateDAO";
 import RegisterUser from "./pages/RegisterUser/RegisterUser";
+import { DynamicContextProvider } from "@dynamic-labs/sdk-react-core";
+import Navbar from "./components/Navbar/Navbar";
+import { EthereumWalletConnectors } from "@dynamic-labs/ethereum";
 import "./App.css";
+
+const evmNetworks = [
+  {
+    blockExplorerUrls: ["https://blockscout.chiadochain.net"],
+    chainId: 10200,
+    chainName: "Gnosis Chaido Testnet",
+    iconUrls: [
+      "https://openseauserdata.com/files/bf23594802bc31b77dafeb42705a686a.jpg",
+    ],
+    name: "Gnosis Chaido Testnet",
+    nativeCurrency: {
+      decimals: 18,
+      name: "xDAI",
+      symbol: "xDAI",
+    },
+    networkId: 10200,
+
+    rpcUrls: ["https://rpc.chiadochain.net"],
+    vanityName: "Gnosis Chaido Testnet",
+  },
+];
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/create-dao" element={<CreateDAO />} />
-        <Route path="/register" element={<RegisterUser />} />
-      </Routes>
-    </Router>
+    <DynamicContextProvider
+      settings={{
+        environmentId: "93021a16-f70d-49ff-b5c4-e0b4ad6bff7e",
+        walletConnectors: [EthereumWalletConnectors],
+        overrides: { evmNetworks },
+      }}
+    >
+      <Navbar />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/create-dao" element={<CreateDAO />} />
+          <Route path="/register" element={<RegisterUser />} />
+        </Routes>
+      </Router>
+    </DynamicContextProvider>
   );
 }
 
