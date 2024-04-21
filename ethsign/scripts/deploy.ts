612c1de0e7b380b0c94bc1c159343c634a8b311c
Mukul Kolpe <mukulkolpe45@gmail.com>
2024-04-21 16:47:06 +0530

    created on chain attestation

--- ethsign/scripts/deploy.ts
+++ ethsign/scripts/deploy.ts
@@ -1,10 +1,10 @@
 import {ethers, upgrades} from 'hardhat'
 
 async function main() {
-    const Factory = await ethers.getContractFactory('Counter')
+    const Factory = await ethers.getContractFactory('UserSide')
     const instance = await Factory.deploy([]),
     const contract = await instance.waitForDeployment()
-    console.log('Counter',await contract.getAddress());
+    console.log('UserSide:',await contract.getAddress());
 
 }
 
